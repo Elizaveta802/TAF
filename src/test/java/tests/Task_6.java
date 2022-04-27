82f61c4ae65c91554e932e33f5f9897d8fdbcead
lenovo <elizaveta.naumovich@mail.ru>
2022-04-27 19:00:30 +0300

    yes

--- src/test/java/tests/Task_6.java
+++ src/test/java/tests/Task_6.java
@@ -3,13 +3,12 @@ package tests;
 import configuration.ReadProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import services.BrowsersService;
-
-import java.time.Duration;
 
 public class Task_6 {
 
@@ -27,19 +26,21 @@ public class Task_6 {
         public void basicLocatorTest() {
             driver.get(ReadProperties.getUrl());
 
-            // Find webElement by ID
             driver.findElement(By.id("user-name")).sendKeys(ReadProperties.username());
 
-            // Find webElement by Name
             driver.findElement(By.name("password")).sendKeys(ReadProperties.password());
 
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-            //Поиск по className
             driver.findElement(By.cssSelector(".submit-button")).click();
 
-            //Поиск по xpath
-            driver.findElement(By.xpath("button[@data-test='add-to-cart-sauce-labs-backpack']")).click();
+            driver.findElement(By.xpath("//button[@data-test='add-to-cart-sauce-labs-backpack']")).click();
+
+            driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+
+            WebElement basketButton = driver.findElement(By.cssSelector("div.inventory_item_price"));
+            Assert.assertEquals(basketButton.getText(), "$29.99");
+
+            WebElement itemName = driver.findElement(By.cssSelector(".inventory_item_name"));
+            Assert.assertEquals(itemName.getText(), "Sauce Labs Backpack");
 
 
 
