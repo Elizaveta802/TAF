6f5810145b6bd20090b839fcf4d1263628bd670b
lenovo <elizaveta.naumovich@mail.ru>
2022-06-05 19:50:18 +0300

    HW+10.Тонкости WebDriver

--- src/test/java/tests/HerokuAppTest.java
+++ src/test/java/tests/HerokuAppTest.java
@@ -3,6 +3,7 @@ package tests;
 import baseEntities.BaseTest;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -81,5 +82,48 @@ public class HerokuAppTest extends BaseTest {
         fileUploadPath.sendKeys("E:\\java\\lessons\\TAF\\src\\test\\resources\\text1.txt");
         wait.waitForExists(By.id("file-submit")).submit();
         Assert.assertEquals(wait.waitForVisibilityLocatedBy(By.id("uploaded-files")).getText(), "text1.txt");
+    }
+
+    @Test
+    public void FramesTest(){
+        driver.get("http://the-internet.herokuapp.com/iframe");
+
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
+        driver.switchTo().frame(driver.findElement(By.cssSelector(".tox-edit-area__iframe")));
+
+        Assert.assertEquals(wait.waitForVisibilityLocatedBy(By.cssSelector("[id='tinymce']")).getText(), "Your content goes here.");
+    }
+
+    @Test
+    public void JavaScriptAlertsTest(){
+        driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
+
+        WebElement clickForJSConfirmLocator = wait.waitForVisibilityLocatedBy(By.cssSelector("[onclick='jsConfirm()']"));
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("arguments[0].click();", clickForJSConfirmLocator);
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        Assert.assertEquals(wait.waitForVisibilityLocatedBy(By.cssSelector("[id='result']")).getText(), "You clicked: Ok");
+    }
+
+    @Test
+    public void JavaScriptExecutorTest(){
+        driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("history.go(0)");
+
+        String url=  javascriptExecutor.executeScript("return document.URL;").toString();
+        System.out.println(url);
+
+        String domainName=  javascriptExecutor.executeScript("return document.domain;").toString();
+        System.out.println(domainName);
+
+        javascriptExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
     }
 }
