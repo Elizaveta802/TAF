12d510c26b84a6fd90b1a30928c3648554fc38ef
lenovo <elizaveta.naumovich@mail.ru>
2022-06-20 22:01:12 +0300

    kW

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,6 +7,6 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     public SelenideElement username = $(By.id("name"));
-    public SelenideElement password =  $("#password");
+    public SelenideElement password = $("#password");
     public SelenideElement loginButton = $("#button_primary");
 }
